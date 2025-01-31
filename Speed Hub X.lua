7536bd360c203d0447714b0518efdc10de3c0f85
remin899 <dustinstephens715@gmail.com>
2025-01-31 16:47:52 -0500

    Update Speed Hub X.lua

--- Speed Hub X.lua
+++ Speed Hub X.lua
@@ -1,6 +1,6 @@
 local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/GameList.lua"))()
 
-for PlaceID, Execute in pairs(Games) do
+for PlaceID, Execute in pairs(roblox) do
     if PlaceID == game.PlaceId then
         loadstring(game:HttpGet(Execute))()
     end
